bab301fab8c64617af430207ef48bf618cd4abb2
mgsosna <matt.g.sosna@gmail.com>
2020-04-17 18:29:38 -0400

    Fixed not importing db

--- app.py
+++ app.py
@@ -15,10 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'Matt'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 db.init_app(app)
 
 jwt = JWT(app, authenticate, identity)  # /auth
